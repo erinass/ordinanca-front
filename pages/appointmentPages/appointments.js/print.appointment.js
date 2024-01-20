873b1f073d75dcd9cb1a0e81deb566e47ed29bab
erinass <erinasallauka@gmail.com>
2024-01-20 22:11:28 +0100

    fronti i zbukurum pak

--- pages/appointmentPages/appointments.js/print.appointment.js
+++ pages/appointmentPages/appointments.js/print.appointment.js
@@ -11,12 +11,55 @@ document.addEventListener("DOMContentLoaded", function () {
         console.log(data);
 
         if(!data.status || data.status === 200)
-            appointment.innerHTML =  `
-            <p><b>Id: </b> <i>${data.id}</i></p>
-            <p><b>Status: </b> <i>${data.active}</i></p>
-            <p><b>Pacient name: </b> <i>${data.pacientName}</i></p>
-            <p><b>Appoint type: </b> <i>${data.appointmentType}</i></p>
-            <p><b>Appointment booking type: </b> <i>${data.appointmentBookingType}</i></p>`;
+            appointment.innerHTML =  `          
+            <div class="p-4 p-md-5 mb-4 text-white rounded bg-dark">
+            <div class="col-md-6 px-0">
+            <h1 class="display-4 fst-italic">Id: ${data.id} </h1>
+              <h1 class="display-4 fst-italic">Pacient: ${data.pacient.name} </h1>
+            </div>
+          </div>
+            <div class="row mb-2">
+            <div class="col-md-3">
+              <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+                <div class="col p-4 d-flex flex-column position-static">
+                  <strong class="d-inline-block mb-2 text-primary">Appointment Type</strong>
+                  <p class="card-text mb-auto">${data.appointmentType}</p>
+                </div>
+                <div class="col-auto d-none d-lg-block">
+        
+                </div>
+              </div>
+            </div>
+            <div class="col-md-3">
+              <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+                <div class="col p-4 d-flex flex-column position-static">
+                  <strong class="d-inline-block mb-2 text-success">Appointment Booking Type</strong>
+                  <p class="mb-auto">${data.appointmentBookingType}</p>
+                </div>
+                <div class="col-auto d-none d-lg-block">
+        
+                </div>
+              </div>
+            </div>
+            <div class="col-md-3">
+              <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+                <div class="col p-4 d-flex flex-column position-static">
+                  <strong class="d-inline-block mb-2 text-danger">Completed</strong>
+                  <p class="mb-auto">${data.active? 'Completed': 'Pending'}</p>
+                </div>
+                <div class="col-auto d-none d-lg-block">
+        
+                </div>
+              </div>
+            </div>
+            
+        </div>
+        <div class="col-md-12">
+                <div class="col-auto d-none d-lg-block">
+        
+                </div>
+              </div>
+            </div>`;
     else
         alert(data.message)
 
